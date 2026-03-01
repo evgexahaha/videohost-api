e600bdc700854816169fc789decc8bc7e96ae2a2
evgexahaha <evgkrug2007@gmail.com>
2026-03-01 13:03:08 +0300

    Add a1238201.xsph.ru to CORS allowed origins

--- config/cors.php
+++ config/cors.php
@@ -5,6 +5,7 @@ return [
     'allowed_methods' => ['*'],
     'allowed_origins' => [
         'https://evgexahaha.github.io',
+        'http://a1238201.xsph.ru',
         'http://localhost:5173',
         'http://localhost:5174',
         'http://127.0.0.1:5173',
